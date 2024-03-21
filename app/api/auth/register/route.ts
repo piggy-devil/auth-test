213513743d361935c42db019c5ef8edf82d78972
piggy-devil <muupaza@gmail.com>
2024-03-21 11:18:10 +0700

    credentials

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+import { sql } from "@vercel/postgres";
+
+export async function POST(request: Request) {
+  try {
+    const { email, password } = await request.json();
+    // YOU MAY WANT TO ADD SOME VALIDATION HERE
+
+    console.log({ email, password });
+
+    const hashedPassword = await hash(password, 10);
+
+    await sql`INSERT INTO users (email, password) VALUES (${email}, ${hashedPassword})`;
+  } catch (e) {
+    console.log({ e });
+  }
+
+  return NextResponse.json({ message: "success" });
+}
